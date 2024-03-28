6394b17fcb2a84321f48331285ab03fb24da4002
Michael E. Johnson <father@bigattichouse.com>
2024-03-28 11:22:42 -0500

    Remove unneeded line from magnitude

--- bitvector.c
+++ bitvector.c
@@ -17,7 +17,6 @@ Explained: https://tekpool.wordpress.com/category/bit-count/
 unsigned int magnitude(unsigned int u)
     {
          unsigned int uCount;
-
          uCount = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111);
          return ((uCount + (uCount >> 3)) & 030707070707) % 63;
     }
